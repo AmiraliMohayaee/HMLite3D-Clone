a02e61815d18e871ca17b287169e65a6215a8936
AmiraliMohayaee <amig90@yahoo.ci>
2020-01-16 20:48:07 +0000

    16/01 Changes

--- Handmade/Player.cpp
+++ Handmade/Player.cpp
@@ -12,7 +12,7 @@ Player::Player()
 	m_yRot = 0.0f;
 	m_zRot = 0.0f;
 
-	m_angle = 20.0f;
+	m_angle = 0.0f;
 }
 
 bool Player::Create()
@@ -45,22 +45,27 @@ void Player::Update()
 	{
 		m_yPos += 0.05f;
 	}
+	else if (TheInput::Instance()->GetRightButtonState() == InputManager::DOWN)
+	{
+		m_yPos -= 0.05f;
+	}
 
 	glm::vec2 motion = TheInput::Instance()->GetMouseMotion();
 
 	std::cout << "X motion is " << motion.x << std::endl;
 	std::cout << "Y motion is " << motion.y << std::endl;
 
-	//m_xRot = motion.x;
-	//m_yRot = motion.y;
+	m_angle += 0.5f;
+	m_xRot = motion.x;
+	m_yRot = motion.y;
 }
 
 void Player::Draw()
 {
 	//GameObject::Translate(m_xPos, m_yPos, m_zPos);
-	//GameObject::Rotate(m_angle, m_xRot, m_yRot, m_zRot);
 
 	GameObject::SetIdentity();
+	//GameObject::Rotate(m_angle, m_xRot, m_yRot, m_zRot);
 	GameObject::Translate(0.5f + m_xPos, 0.0f + m_yPos, 0.0f + m_zPos);
 	TheDebug::Instance()->DrawSphere3D(0.3f, 100, 90, 100, 0.5f);
 
@@ -73,6 +78,8 @@ void Player::Draw()
 	GameObject::Translate(0.0f + m_xPos, 0.5f + m_yPos, 0.0f + m_zPos);
 	//GameObject::Rotate(m_angle, m_xRot, m_yRot, m_zRot);
 	TheDebug::Instance()->DrawCube3D(0.5f, 1.0f, 0.5f, 100.0f, 100.0f, 100.0f, 0.5f);
+
+	
 }
 
 void Player::Destroy()
